bdf643f4b8b7aec5e31b30e5b35f98ac9e64ba6f
Karan612 <Kjpatel.522@gmail.com>
2020-03-09 18:27:14 -0400

    Uploading Assignment-1

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //get the server and export it 
 var app = require("./server/server.js");
 
-app.listen(3000,()=>{
-    console.log(`GBC Chat is live on port 3000`);
+app.listen(8080,()=>{
+    console.log(`GBC Chat is live on port 8080`);
 })
 //export the chating app
 module.exports = app;
